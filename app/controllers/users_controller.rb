54cb3d7b2778459fcda3e301eefa1bd46925dc3a
shyakaster <shyakaster@gmail.com>
2015-03-26 13:59:14 +0300

    add sign in on registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
+      signin @user
       flash[:success]='Welcome to the sample application!'
       redirect_to @user
     else
